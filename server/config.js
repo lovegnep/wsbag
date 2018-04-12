2adb89f301429e69af83b6850eaffa40fc40ab4a
gnep <lovegnep@163.com>
2018-04-12 18:48:28 +0800

    222

--- server/config.js
+++ server/config.js
@@ -8,6 +8,10 @@ let config = {
     port: 5000,
     file_limit: '10MB',
     visit_per_day: 1000, // 每个 ip 每天能访问的次数
+    redis:{
+        port: 6379,
+        host: 127.0.0.1,
+    }
 };
 
 module.exports = config;
---
+++ server/dataopt/interface.js
@@ -0,0 +1,8 @@
+const Redis = require('redis');
+const Config = require('../config');
+let client = Redis.createClient(Config.redis);
+
+
+exports = {
+    
+}
